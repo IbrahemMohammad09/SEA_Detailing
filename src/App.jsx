ec3c9091d427f4e16465828b51ee702ff6e84d04
Maya_Bishani <mayabishani6@gmail.com>
2025-02-13 10:34:10 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import React from "react";
+import Footer from './assets/Sections/Footer'
 function App() {
 
 
   return (
         <>
-          
+         <Footer/>
         </>
   )
 }
