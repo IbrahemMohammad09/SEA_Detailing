cec37a121f6113bf2a6248d00a9249cad19ef2ea
adeeeb <adeebmoualla3@gmail.com>
2025-02-15 13:59:39 +0300

    Create HeroSection2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import HeroSection1 from "../sections/HeroSection1";
+import HeroSection2 from "../sections/HeroSection2";
 function HomePage() {
   return (
     <>
       <div>
         <Navbar />
         <HeroSection1 />
+        <HeroSection2 />
       </div>
     </>
   );
