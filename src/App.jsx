5c2303e908e854bdf65ab7e2d908beb4114a988d
adeeeb <adeebmoualla3@gmail.com>
2025-02-18 01:45:08 +0300

    Create BookService Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, useState, useEffect } from "react";
 
-const GiftLoader = lazy (() => import('./components/GifLoader'))
+const GiftLoader = lazy(() => import("./components/GifLoader"));
 const HomePage = lazy(() => import("./pages/HomePage"));
-
+const BookService = lazy(() => import("./pages/BookService"));
 function App() {
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false); 
+      setIsLoading(false);
     }, 2200);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -23,6 +23,7 @@ function App() {
       ) : (
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="/book" element={<BookService />} />
         </Routes>
       )}
     </BrowserRouter>
---
+++ src/pages/BookService.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import HeroBookService from "../sections/HeroBookSection";
+import BookNow from "../sections/BookNow";
+import Footer from "../components/Footer";
+function BookService() {
+  return (
+    <>
+      <Navbar />
+      <HeroBookService />
+      <BookNow />
+      <Footer />
+    </>
+  );
+}
+
+export default BookService;
