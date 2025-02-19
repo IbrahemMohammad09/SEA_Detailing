585940ddacd09df6e0528bc061a5b2132981ae28
adeeeb <adeebmoualla3@gmail.com>
2025-02-19 16:14:50 +0300

    Create Error Page and Link Between Pages and Sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { motion } from "framer-motion";
 import Logo from "../assets/logo.png";
+import { HashLink } from "react-router-hash-link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-
+  const scrollWithOffset = (offset) => (el) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = offset;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+  };
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -22,7 +23,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-40 flex items-center justify-between px-10 lg:px-36 xl:px-52 md:px-10  transition-all duration-300 ${
+      className={`fixed top-0 left-0 right-0 z-40 flex items-center justify-between px-10 lg:px-36 xl:px-52 md:px-10 transition-all duration-300 ${
         isScrolled ? "bg-white shadow-lg py-0" : "bg-transparent py-4"
       }`}
     >
@@ -30,7 +31,7 @@ const Navbar = () => {
         <motion.img
           src={Logo}
           alt="Logo"
-          className={`  transition-all duration-300 ${
+          className={`transition-all duration-300 ${
             isScrolled ? "md:h-32 h-16" : "md:h-52 h-20"
           }`}
           initial={{ scale: 1 }}
@@ -55,24 +56,44 @@ const Navbar = () => {
       {/* القائمة للشاشات الكبيرة */}
       <ul className="hidden md:flex text-2xl z-40 sm:space-x-10 md:space-x-20 text-gray-800 abhaya-libre-extrabold">
         <li>
-          <a href="#" className="hover:text-[#1A78F2] transition-colors">
+          <HashLink
+            smooth
+            to="/#home"
+            scroll={scrollWithOffset(-100)}
+            className="hover:text-[#1A78F2] transition-colors"
+          >
             Home
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a href="#" className="hover:text-[#1A78F2] transition-colors">
+          <HashLink
+            smooth
+            to="/#our-services"
+            scroll={scrollWithOffset(-80)}
+            className="hover:text-[#1A78F2] transition-colors cursor-pointer"
+          >
             Our Services
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a href="#" className="hover:text-[#1A78F2] transition-colors">
+          <HashLink
+            smooth
+            to="/#contact-us"
+            scroll={scrollWithOffset(-100)}
+            className="hover:text-[#1A78F2] transition-colors cursor-pointer"
+          >
             Contact Us
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a href="#" className="hover:text-[#1A78F2] transition-colors">
+          <HashLink
+            smooth
+            to="/#about-us"
+            scroll={scrollWithOffset(-150)}
+            className="hover:text-[#1A78F2] transition-colors cursor-pointer"
+          >
             About Us
-          </a>
+          </HashLink>
         </li>
       </ul>
 
@@ -81,44 +102,50 @@ const Navbar = () => {
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -20 }}
         transition={{ duration: 0.3 }}
-        className={`absolute z-40  left-[10%] rounded-2xl h-48 text-center w-[80%] text-xl bg-white shadow-md md:hidden text-gray-800 
+        className={`absolute z-40 left-[10%] rounded-2xl h-48 text-center w-[80%] text-xl bg-white shadow-md md:hidden text-gray-800 
           ${isScrolled ? "top-16" : "top-24"}
           ${isOpen ? "block" : "hidden"}
-        
-
         `}
       >
         <li>
-          <a
-            href="#"
+          <HashLink
+            smooth
+            onClick={() => setIsOpen(false)}
+            to="/"
             className="block py-2 px-4 hover:bg-[#1A78F2] hover:rounded-3xl hover:text-white hover:font-bold transition-colors"
           >
             Home
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a
-            href="#"
+          <HashLink
+            smooth
+            onClick={() => setIsOpen(false)}
+            to="/#our-services"
             className="block py-2 px-4 hover:bg-[#1A78F2] hover:rounded-3xl hover:text-white hover:font-bold transition-colors"
           >
             Our Services
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a
-            href="#"
+          <HashLink
+            smooth
+            onClick={() => setIsOpen(false)}
+            to="/#contact-us"
             className="block py-2 px-4 hover:bg-[#1A78F2] hover:rounded-3xl hover:text-white hover:font-bold transition-colors"
           >
             Contact Us
-          </a>
+          </HashLink>
         </li>
         <li>
-          <a
-            href="#"
+          <HashLink
+            smooth
+            onClick={() => setIsOpen(false)}
+            to="/#about-us"
             className="block py-2 px-4 hover:bg-[#1A78F2] hover:rounded-3xl hover:text-white hover:font-bold transition-colors"
           >
             About Us
-          </a>
+          </HashLink>
         </li>
       </motion.ul>
     </nav>
