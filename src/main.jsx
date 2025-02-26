6e7c34ee624e076e984fb193f79ad49a9a952ffd
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-02-26 06:01:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { HelmetProvider } from "react-helmet-async";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
+
 
 import './index.css'
 import App from './App.jsx'
